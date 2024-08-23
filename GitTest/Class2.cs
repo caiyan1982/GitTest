018eb4a97cdd3d7d3de4178f7be4a2323b57456a
caiyan <caiyan1982@hotmail.com>
2024-08-23 16:52:51 +0800

    Class2类中增加FuncA方法

--- GitTest/Class2.cs
+++ GitTest/Class2.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace GitTest
 {
     class Class2
     {
+        public void FuncA()
+        { }
     }
 }
